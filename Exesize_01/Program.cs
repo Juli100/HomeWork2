eaa18100275feabbb3b276be28bf48870ad709fc
Yulia Batrakova <julichka_71@mail.ru>
2022-01-06 00:10:42 +0300

    Kub added

---
+++ Exesize_01/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine()?? "0");
+for (int i = 1; i <= N; i++)
+ //i <= k; i++)
+{
+    Console.WriteLine($"{i} = {i * i * i}");
+}
+
