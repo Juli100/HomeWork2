b653e94da59341fc75db58fe323c05afd10ab7bb
Yulia Batrakova <julichka_71@mail.ru>
2022-01-06 23:06:50 +0300

    Sum added

---
+++ Exesize_01/Exersize_04/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите степень");
+int B = int.Parse(Console.ReadLine());
+
+int index = 1;
+int result = 0;
+
+while (index < 0)
+(
+    index = result * A;
+    index++;    
+)
+Console.WriteLine(result);
