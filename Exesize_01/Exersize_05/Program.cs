200e500122f4fa30a60e22cde43fdc74d3e460a6
Yulia Batrakova <julichka_71@mail.ru>
2022-01-06 23:03:46 +0300

    Work added

---
+++ Exesize_01/Exersize_05/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+int index = 1;
+int work = 1;
+
+while (index <= N)
+{
+    work = work * index;
+    index++;
+}
+Console.WriteLine($"произведение чисел от 1 до {N} = {work}");
