9eaa980fce3626b5a5ed70f0fea30495709ceffd
Yulia Batrakova <julichka_71@mail.ru>
2022-01-06 14:36:33 +0300

    Exes 3 added

---
+++ Exesize_01/Exersize_03/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+int index = 1;
+int sum = 0;
+
+while (index <= N)
+{
+    sum = sum + index;
+    index++;
+}
+Console.WriteLine($"сумма чисел от 1 до {N} = {sum}");
