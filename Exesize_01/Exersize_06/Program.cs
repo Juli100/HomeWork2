adc51c77a79b9da041bc26703998243ddf423c27
Yulia Batrakova <julichka_71@mail.ru>
2022-01-07 00:00:06 +0300

    Kub chetnoe added

---
+++ Exesize_01/Exersize_06/Program.cs
@@ -0,0 +1,13 @@
+﻿//int kub = new Random().Next(1, 10);
+Console.WriteLine("введите число: ");
+int kub = int.Parse(Console.ReadLine());
+Console.WriteLine($"{kub} = {kub * kub * kub}");
+int N = kub;
+if ( N % 2 == 0 )
+{
+    Console.WriteLine("куб числа четное");
+}
+if (N % 2 == 1)
+{
+    Console.WriteLine("не четное ");
+}
